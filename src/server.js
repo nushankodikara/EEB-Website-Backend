bf748c66bba6770ed5920eae7217183da51dee5a
nushankodikara <nushankodi@gmail.com>
2023-01-24 22:55:28 +0530

    added cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+var cors = require("cors");
 let bodyParser = require("body-parser");
 let multer = require("multer");
 let forms = multer();
@@ -8,6 +8,24 @@ let forms = multer();
 require("dotenv").config();
 
 // apply them
+
+var allowedOrigins = ["http://localhost:5173", "https://euroeximbank.com"];
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			// allow requests with no origin
+			// (like mobile apps or curl requests)
+			if (!origin) return callback(null, true);
+			if (allowedOrigins.indexOf(origin) === -1) {
+				var msg =
+					"The CORS policy for this site does not " +
+					"allow access from the specified Origin.";
+				return callback(new Error(msg), false);
+			}
+			return callback(null, true);
+		},
+	})
+);
 
 app.use(bodyParser.json());
 app.use(forms.array());
