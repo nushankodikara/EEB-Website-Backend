41b99686e7dee7197cf329517e5c89e2bb05c781
nushankodikara <nushankodi@gmail.com>
2023-05-03 08:04:19 +0530

    few updates

--- src/server.js
+++ src/server.js
@@ -10,26 +10,26 @@ require("dotenv").config();
 // apply them
 
 var allowedOrigins = [
-	"http://localhost:5173",
-	"https://euroeximbank.com",
-	"https://www.euroeximbank.com",
-	"https://eeb-website.vercel.app",
+    "http://localhost:5173",
+    "https://euroeximbank.com",
+    "https://www.euroeximbank.com",
+    "https://eeb-website.vercel.app",
 ];
 app.use(
-	cors({
-		origin: function (origin, callback) {
-			// allow requests with no origin
-			// (like mobile apps or curl requests)
-			if (!origin) return callback(null, true);
-			if (allowedOrigins.indexOf(origin) === -1) {
-				var msg =
-					"The CORS policy for this site does not " +
-					"allow access from the specified Origin.";
-				return callback(new Error(msg), false);
-			}
-			return callback(null, true);
-		},
-	})
+    cors({
+        origin: function (origin, callback) {
+            // allow requests with no origin
+            // (like mobile apps or curl requests)
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                var msg =
+                    "The CORS policy for this site does not " +
+                    "allow access from the specified Origin.";
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        },
+    })
 );
 
 app.use(bodyParser.json());
@@ -42,13 +42,18 @@ const port = process.env.PORT || 3000;
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
 
-connection.connect();
+try {
+    connection.connect();
+    console.log("Database Connected");
+} catch (error) {
+    console.log("Database Connection Failed", error);
+}
 
 // connection.connect();
 
@@ -66,30 +71,30 @@ app.get("/", (req, res) => res.send("Server Is Running."));
 
 // get email from post request
 app.post("/check", (req, res) => {
-	let check = req.body.check || "";
+    let check = req.body.check || "";
 
-	// search database
-	if (check == "" || check == null) {
-		res.send({ id: -99, message: "Enter email or mobile number" });
-	} else {
-		try {
-			let sql = `SELECT * FROM demo WHERE email="${check}" OR empnumber="${check}" OR mobile="${check}"`;
+    // search database
+    if (check == "" || check == null) {
+        res.send({ id: -99, message: "Enter email or mobile number" });
+    } else {
+        try {
+            let sql = `SELECT * FROM demo WHERE email="${check}" OR empnumber="${check}" OR mobile="${check}"`;
 
-			connection.query(sql, function (err, result) {
-				if (err) throw err;
-				if (result.length == 0) {
-					console.log("No Result Found");
-					res.send({ id: -99, message: "No record found" });
-				} else {
-					console.log(result[0]);
-					res.send(result[0]);
-				}
-			});
-		} catch (error) {
-			console.log(error);
-			res.send({ id: -99, message: "Something went wrong" });
-		}
-	}
+            connection.query(sql, function (err, result) {
+                if (err) throw err;
+                if (result.length == 0) {
+                    console.log("No Result Found");
+                    res.send({ id: -99, message: "No record found" });
+                } else {
+                    console.log(result[0]);
+                    res.send(result[0]);
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            res.send({ id: -99, message: "Something went wrong" });
+        }
+    }
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
